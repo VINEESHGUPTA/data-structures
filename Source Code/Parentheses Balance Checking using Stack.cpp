645ef1d269c93dacea39d4c36037af4c1a9c9f54
Vineesh Gupta <vineeshgupta898@gmail.com>
2019-10-10 22:31:00 +0530

    Update Parentheses Balance Checking using Stack.cpp

--- Source Code/Parentheses Balance Checking using Stack.cpp
+++ Source Code/Parentheses Balance Checking using Stack.cpp
@@ -3,31 +3,76 @@ Parentheses Balance checking using STACK data structure in C++
 Code written by Hasan Abdullah
 Contact: hellohasan.com
 */
-
-#include<bits/stdc++.h>
-
-using namespace std;
-
-int main()
-{
-    char str[10] = "((())())";
-
-    stack<char> myStack;
-
-    for(int i=0; str[i]; i++)
-    {
-        if(!myStack.empty() && myStack.top()=='(' && str[i]==')')
-            myStack.pop();
-        else
-            myStack.push(str[i]);
-    }
-
-    if(myStack.empty())
-        printf("%s is Balanced\n", str);
+// CPP program to check for balanced parenthesis. 
+#include<bits/stdc++.h> 
+using namespace std; 
+  
+// function to check if paranthesis are balanced 
+bool areParanthesisBalanced(string expr) 
+{ 
+    stack<char> s; 
+    char x; 
+  
+    // Traversing the Expression 
+    for (int i=0; i<expr.length(); i++) 
+    { 
+        if (expr[i]=='('||expr[i]=='['||expr[i]=='{') 
+        { 
+            // Push the element in the stack 
+            s.push(expr[i]); 
+            continue; 
+        } 
+  
+        // IF current current character is not opening 
+        // bracket, then it must be closing. So stack 
+        // cannot be empty at this point. 
+        if (s.empty()) 
+           return false; 
+  
+        switch (expr[i]) 
+        { 
+        case ')': 
+  
+            // Store the top element in a 
+            x = s.top(); 
+            s.pop(); 
+            if (x=='{' || x=='[') 
+                return false; 
+            break; 
+  
+        case '}': 
+  
+            // Store the top element in b 
+            x = s.top(); 
+            s.pop(); 
+            if (x=='(' || x=='[') 
+                return false; 
+            break; 
+  
+        case ']': 
+  
+            // Store the top element in c 
+            x = s.top(); 
+            s.pop(); 
+            if (x =='(' || x == '{') 
+                return false; 
+            break; 
+        } 
+    } 
+  
+    // Check Empty Stack 
+    return (s.empty()); 
+} 
+  
+// Driver program to test above function 
+int main() 
+{ 
+    string expr = "{()}[]"; 
+  
+    if (areParanthesisBalanced(expr)) 
+        cout << "Balanced"; 
     else
-        printf("%s is Not-balanced\n", str);
-
-
-    return 0;
-}
+        cout << "Not Balanced"; 
+    return 0; 
+} 
 
